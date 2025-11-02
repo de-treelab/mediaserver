578596c00e23044d98431ee3905702af3d7e4548
Lars Hagemann <hagemann.l@outlook.com>
2025-11-02 22:45:32 +0100

    fix: untrusted plugins

--- frontend/src/components/DocumentRender.tsx
+++ frontend/src/components/DocumentRender.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { useDocument } from "../hooks/useDocument";
 import { useDocumentPlugin } from "../hooks/useDocumentPlugin";
-import { useIsPluginTrusted } from "../hooks/useIsPluginTrusted";
 
 type Props = {
   documentId: string;
@@ -11,17 +10,9 @@ export const DocumentRender = ({ documentId }: Props) => {
   const { objectUrl, blob } = useDocument(documentId);
   const plugin = useDocumentPlugin(blob?.type);
 
-  const isPluginTrusted = useIsPluginTrusted(plugin);
-
   if (!blob || !objectUrl) {
-    return <div>Cannot render document</div>;
+    return null;
   }
 
-  return isPluginTrusted ? (
-    <plugin.Render objectUrl={objectUrl} React={React} />
-  ) : (
-    <iframe className="w-full h-full">
-      <plugin.Render objectUrl={objectUrl} React={React} />
-    </iframe>
-  );
+  return <plugin.Render objectUrl={objectUrl} React={React} />;
 };
